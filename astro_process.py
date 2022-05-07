ff7fa9014131ef1416f452081de075a766dfcbba
Matthieu Brucher <matthieu.brucher@gmail.com>
2022-05-07 14:20:37 +0100

    Add deconvolve

--- astro_process.py
+++ astro_process.py
@@ -22,6 +22,13 @@ def histogramSmoothing(channel, hist_smoothing, sky_level_factor):
     channel = (channel - skybin/NB_BINS) / (1-skybin/NB_BINS)
     return channel
 
+def deconvolve(img, radius, sigma):
+    y, x = np.mgrid[-radius:radius + 1, -radius:radius + 1]
+    weights = np.exp(-0.5 * (x ** 2 + y ** 2) / sigma ** 2)
+    weights /= 2 * np.pi * sigma * sigma
+
+    return skimage.restoration.richardson_lucy(img, weights[:,:,None])
+
 if __name__ == "__main__":
     import argparse
     import os
@@ -31,12 +38,15 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process Astrophotography')
     parser.add_argument('--input', type=str, required=True, help='Input image name')
     parser.add_argument('--output', type=str, required=True, help='Output image name')
-    parser.add_argument('--pre-sky-output', type=str, help='Pre sky level adjustments image name')
+    parser.add_argument('--power-adjustment-output', type=str, help='Output image name after power adjustment')
+    parser.add_argument('--sky-removal-output', type=str, help='Output image name after sky removal')
     parser.add_argument('--root-power', type=float, help='Histogram stretch factor')
     parser.add_argument('--hist-smoothing', type=int, help='Histogram smoothing')
     parser.add_argument('--sky-level-factor', type=float, help='Factor for the sky level')
     parser.add_argument('--sky-removal-passes', type=int, help='Number of sky removal passes')
-    parser.set_defaults(root_power=0.4, sky_level_factor=0.6, hist_smoothing=601, sky_removal_passes=2)
+    parser.add_argument('--radius', type=int, help='Gaussian deconvolve kernal radius')
+    parser.add_argument('--sigma', type=int, help='Gaussian deconvolve kernal sigma')
+    parser.set_defaults(root_power=0.6, sky_level_factor=0.6, hist_smoothing=601, sky_removal_passes=2, radius=7, sigma=2)
 
     args = parser.parse_args()
 
@@ -45,9 +55,9 @@ if __name__ == "__main__":
     img = np.asarray(img) / np.iinfo(np.asarray(img).dtype).max
         
     img = np.power(img, args.root_power)
-    if args.pre_sky_output:
-        logging.info(f"Saving pre-sky adjustment file {args.pre_sky_output}")
-        skimage.io.imsave(args.pre_sky_output, (img * np.iinfo(np.uint16).max).astype(np.uint16), plugin='tifffile')
+    if args.power_adjustment_output:
+        logging.info(f"Saving pre-sky adjustment file {args.power_adjustment_output}")
+        skimage.io.imsave(args.power_adjustment_output, (img * np.iinfo(np.uint16).max).astype(np.uint16), plugin='tifffile')
 
     for i in range(args.sky_removal_passes):
         logging.info(f"Computing histogram")
@@ -55,6 +65,13 @@ if __name__ == "__main__":
         img[:,:,1] = histogramSmoothing(img[:,:,1].reshape(-1), args.hist_smoothing, args.sky_level_factor).reshape(img.shape[:-1])
         img[:,:,2] = histogramSmoothing(img[:,:,2].reshape(-1), args.hist_smoothing, args.sky_level_factor).reshape(img.shape[:-1])
         img = np.clip(img, 0, 1)
+
+    if args.sky_removal_output:
+        logging.info(f"Saving pre-sky adjustment file {args.sky_removal_output}")
+        skimage.io.imsave(args.sky_removal_output, (img * np.iinfo(np.uint16).max).astype(np.uint16), plugin='tifffile')
+
+    logging.info(f"Deconvolving image")
+    img = deconvolve(img, args.radius, args.sigma)
 
     logging.info(f"Saving output file {args.output}")
     skimage.io.imsave(args.output, (img * np.iinfo(np.uint16).max).astype(np.uint16), plugin='tifffile')
